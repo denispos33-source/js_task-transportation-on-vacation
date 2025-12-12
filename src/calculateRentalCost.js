3ffd5d8f9a35debda1efe409efaeae72390eb536
Kucheriaviy Denis <denispos33@gmail.com>
2025-12-12 03:27:56 -1200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,12 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let totalCost = days * 40;
+  const cost = 40;
+  const LONG_TERM = 7;
+  const MEDIUM_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const MEDIUM_TERM_DISCOUNT = 20;
+  let totalCost = days * cost;
 
-  if (days >= 7) {
-    totalCost -= 50;
-  } else if (days >= 3) {
-    totalCost -= 20;
+  if (days >= LONG_TERM) {
+    totalCost -= LONG_TERM_DISCOUNT;
+  } else if (days >= MEDIUM_TERM) {
+    totalCost -= MEDIUM_TERM_DISCOUNT;
   }
 
   return totalCost;
